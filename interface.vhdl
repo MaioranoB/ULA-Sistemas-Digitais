777617072ac84cd9fdc0ede815347ce467babd0c
MaioranoB <54644125+MaioranoB@users.noreply.github.com>
2020-10-03 16:29:16 -0300

    Update interface.vhdl

--- interface.vhdl
+++ interface.vhdl
@@ -5,11 +5,11 @@ use ieee.std_logic_1164.all;
 
 entity interface is
 port (clk, reset, botaoSEL : in std_logic; --botaoSEL carrega A e B simultaneamente
-		entradaA, entradaB : in std_logic_vector (3 downto 0); -- switches 
-		resultadoDISPLAY	  : out std_logic_vector(3 downto 0); --display pra resposta 
+		entradaA, entradaB : in std_logic_vector (3 downto 0); -- switches
+		resultadoDISPLAY	  : out std_logic_vector(3 downto 0); --display pra A e B tbm
 		carry_borrowLED : out std_logic;
-		operacaoLED : out std_logic_vector(2 downto 0);
-		saidaA, saidaB : out std_logic_vector(3 downto 0) -- saida para display A e display B
+		operacaoLED : out std_logic_vector(2 downto 0)
+		saidaA, saidaB : out std_logic_vector (3 downto 0) --
 );
 end interface;
 
@@ -22,7 +22,6 @@ architecture behav of interface is
 				carry_borrow_OUT : out std_logic
 		);
 	end component;
-	
 		
 	signal A, B, result: std_logic_vector(3 downto 0) := "0000";
 	signal carry_borrow: std_logic := '0';
@@ -33,25 +32,22 @@ architecture behav of interface is
 begin
 	alu: ULA port map (A, B, operacao, result, carry_borrow);
 	
+	saidaA <= A;
+	saidaB <= B;
 	resultadoDISPLAY <= result;
 	operacaoLED <= operacao;
 	carry_borrowLED <= carry_borrow;
-
+	
 	process(clk, botaoSEL, reset)
-		--variable op: std_logic_vector(2 downto 0) := "000";
 		begin
-			if reset = '0' then 			-- botao manda 0 quando eh apertado
+			if (reset = '1') then 	--AJEITAR A LOGICA DO BOTAO DPS!!!
 				estado <= entrada;
 				A <= "0000";
 				B <= "0000";
 				operacao <= "000";
-			elsif (estado = entrada and botaoSEL = '1') then
-				saidaA <= entradaA;
-				saidaB <= entradaB;
-			elsif (estado = entrada and botaoSEL = '0')then --botao manda 0 quando eh apertado
+			elsif (estado = entrada and botaoSEL = '1')then --botao manda 0 quando eh apertado!! mudar isso dps
+			--elsif (botaoSEL = '1' and operacao = "000")then --botao manda 0 quando eh apertado!! mudar isso dps
 				estado <= saida;
-				saidaA <= entradaA;
-				saidaB <= entradaB;
 				A <= entradaA;
 				B <= entradaB;
 				operacao <= "000";
@@ -73,8 +69,9 @@ begin
 						operacao <= "111";
 					elsif operacao = "111" then
 						operacao <= "000";
+					else
+						operacao <= "000";
 					end if;
 			end if;
 	end process;
-
 end behav;
