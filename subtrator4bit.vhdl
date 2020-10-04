08547e5e618a9ceb1282830489c4dac3174d9dcb
Thiago Guimarães <thiaoguimaraes.dev@gmail.com>
2020-10-04 18:00:42 -0300

    moving to complement 2 implementation

--- subtrator4bit.vhdl
+++ subtrator4bit.vhdl
@@ -1,23 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity subtrator1bit is
-port (
-		a,b,bIN 	 : in  std_logic;
-		diff,bOUT : out std_logic
-);
-end subtrator1bit;
-
-architecture behav of subtrator1bit is
-begin
-	diff <= a xor b xor bIN;
-	bOUT <= (not a and b) or (not a and bIN) or (b and bIN);
-end behav;
-
-
-library ieee;
-use ieee.std_logic_1164.all;
-
 entity subtrator4bit is
 port (a,b  		 : in std_logic_vector(3 downto 0);
 		borrowIN  : in std_logic;
@@ -27,20 +10,20 @@ port (a,b  		 : in std_logic_vector(3 downto 0);
 end subtrator4bit;
 
 architecture structure of subtrator4bit is
-	
-	--signal borrow: std_logic_vector(4 downto 0);
-	signal b_aux1,b_aux2,b_aux3 : std_logic; --borrow's parciais
-	
-	component subtrator1bit
-		port (a,b,Bin   : in std_logic;
-				diff,Bout : out std_logic
+	component somador4bit
+		port (a,b      : in std_logic_vector(3 downto 0);
+				carryIN  : in std_logic;
+				sum      : out std_logic_vector(3 downto 0);
+				carryOUT : out std_logic
 		);
 	end component;
 	
+	signal invertedB : std_logic_vector(3 downto 0) := not b;
+	signal somadorCarryOut: std_logic;
+	
 	begin
-	FS0: subtrator1bit port map (a(0), b(0), borrowIN,  diff(0), b_aux1);
-	FS1: subtrator1bit port map (a(1), b(1), b_aux1,    diff(1), b_aux2);
-	FS2: subtrator1bit port map (a(2), b(2), b_aux2,    diff(2), b_aux3);
-	FS3: subtrator1bit port map (a(3), b(3), b_aux3,    diff(3), borrowOUT);
+	
+	subtraction: somador4bit port map (a, invertedB, '1', diff, somadorCarryOut);
+	borrowOUT <= not somadorCarryOut;
 	
 end structure;
